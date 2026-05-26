2004e1dc6eecbf00a52d0130bf59b606ba45040b
Midrax <rexridam@gmail.com>
2026-05-26 01:54:33 +0100

    Turned multiple planes to single one with dynamic material

--- Source/TheOtleyRun/Private/MaskActor.cpp
+++ Source/TheOtleyRun/Private/MaskActor.cpp
@@ -1,329 +1,246 @@
 #include "MaskActor.h"
+
 #include "Components/StaticMeshComponent.h"
 #include "UObject/ConstructorHelpers.h"
 
-// Sets default values
 AMaskActor::AMaskActor()
 {
-    PrimaryActorTick.bCanEverTick = true;
-
-    // Root
+    PrimaryActorTick.bCanEverTick = false;
     Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
     SetRootComponent(Root);
-
-    // Base Mesh
     BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
     BaseMesh->SetupAttachment(Root);
-
-    // Face Root (holds all planes)
-    FaceRoot = CreateDefaultSubobject<USceneComponent>(TEXT("FaceRoot"));
-    FaceRoot->SetupAttachment(Root);
-    FaceRoot->SetMobility(EComponentMobility::Movable);
-
-    // Face Planes
-    BrowsPlane  = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BrowsPlane"));
-    EyesPlane   = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("EyesPlane"));
-    NosePlane  = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("NosePlane"));
-    MouthPlane  = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MouthPlane"));
-    AccessoryPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AccessoryPlane"));
-    SymbolPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SymbolPlane"));
-    DetailPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DetailPlane"));
-
-    BrowsPlane->SetupAttachment(FaceRoot);
-    EyesPlane->SetupAttachment(FaceRoot);
-    NosePlane->SetupAttachment(FaceRoot);
-    MouthPlane->SetupAttachment(FaceRoot);
-    AccessoryPlane->SetupAttachment(FaceRoot);
-    SymbolPlane->SetupAttachment(FaceRoot);
-    DetailPlane->SetupAttachment(FaceRoot);
-
-    // Default meshes for viewport preview
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneMesh(TEXT("/Engine/BasicShapes/Plane.Plane"));
-
-    if (SphereMesh.Succeeded())
-    {
-        BaseMesh->SetStaticMesh(SphereMesh.Object);
-    }
-
+    FacePlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FacePlane"));
+    FacePlane->SetupAttachment(Root);
+    FacePlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    static ConstructorHelpers::FObjectFinder<UStaticMesh>PlaneMesh(TEXT("/Engine/BasicShapes/Plane.Plane"));
     if (PlaneMesh.Succeeded())
     {
-        BrowsPlane->SetStaticMesh(PlaneMesh.Object);
-        EyesPlane->SetStaticMesh(PlaneMesh.Object);
-        NosePlane->SetStaticMesh(PlaneMesh.Object);
-        MouthPlane->SetStaticMesh(PlaneMesh.Object);
-        AccessoryPlane->SetStaticMesh(PlaneMesh.Object);
-        SymbolPlane->SetStaticMesh(PlaneMesh.Object);
-        DetailPlane->SetStaticMesh(PlaneMesh.Object);
+        FacePlane->SetStaticMesh(PlaneMesh.Object);
     }
-
-    // Disable collisions for planes
-    BrowsPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    EyesPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    NosePlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    MouthPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    AccessoryPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    SymbolPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-    DetailPlane->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
-// Called when properties are changed in editor or actor is spawned
+void AMaskActor::BeginPlay()
+{
+    Super::BeginPlay();
+    CreateFaceMaterial();
+    RefreshAllFaceParts();
+    ApplyHeadMaterial();
+}
+
 void AMaskActor::OnConstruction(const FTransform& Transform)
 {
     Super::OnConstruction(Transform);
-
-    if (HasAnyFlags(RF_ClassDefaultObject)) return;
-
-    ApplyBaseShape();
-
-    if (bAutoLayout)
-    {
-        UpdateFaceOffsets();
-    }
-
-    // Delay material reapply
-    bNeedsMaterialRefresh = true;
-
-    // Reapply textures safely
-    SetFacePart(EFacePartCategory::Brows, BrowsTexture);
-    SetFacePart(EFacePartCategory::Eyes, EyesTexture);
-    SetFacePart(EFacePartCategory::Nose, NoseTexture);
-    SetFacePart(EFacePartCategory::Mouth, MouthTexture);
-    SetFacePart(EFacePartCategory::Accessory, AccessoryTexture);
-    SetFacePart(EFacePartCategory::Symbol, SymbolTexture);
-    SetFacePart(EFacePartCategory::Detail, DetailTexture);
-
+    UpdateFaceTransform();
+    CreateFaceMaterial();
+    RefreshAllFaceParts();
     ApplyHeadMaterial();
 }
 
 #if WITH_EDITOR
+
 void AMaskActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
     Super::PostEditChangeProperty(PropertyChangedEvent);
-
-    Super::PostEditChangeProperty(PropertyChangedEvent);
-
-    if (HasAnyFlags(RF_ClassDefaultObject)) return;
-
-    ApplyBaseShape();
-
-    if (bAutoLayout)
-    {
-        UpdateFaceOffsets();
-    }
-
+    UpdateFaceTransform();
+    CreateFaceMaterial();
+    RefreshAllFaceParts();
     ApplyHeadMaterial();
 }
 
-void AMaskActor::PostRegisterAllComponents()
+void AMaskActor::PostLoad()
 {
-    Super::PostRegisterAllComponents();
+    Super::PostLoad();
 
-    if (HasAnyFlags(RF_ClassDefaultObject)) return;
-
-    if (bNeedsMaterialRefresh)
-    {
-        bNeedsMaterialRefresh = false;
-
-        SetFacePart(EFacePartCategory::Brows, BrowsTexture);
-        SetFacePart(EFacePartCategory::Eyes, EyesTexture);
-        SetFacePart(EFacePartCategory::Nose, NoseTexture);
-        SetFacePart(EFacePartCategory::Mouth, MouthTexture);
-        SetFacePart(EFacePartCategory::Accessory, AccessoryTexture);
-        SetFacePart(EFacePartCategory::Symbol, SymbolTexture);
-        SetFacePart(EFacePartCategory::Detail, DetailTexture);
-    }
-
+    CreateFaceMaterial();
+    RefreshAllFaceParts();
     ApplyHeadMaterial();
+    UpdateFaceTransform();
 }
 
 #endif
 
-// Apply selected base shape
-void AMaskActor::ApplyBaseShape()
+void AMaskActor::UpdateFaceTransform()
 {
-    if (!BaseMesh) return;
-
-    UStaticMesh* MeshToUse = nullptr;
-
-    switch (BaseShape)
+    if (!FacePlane || !BaseMesh)
     {
-        case EMaskBaseShape::Sphere:
-            MeshToUse = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Sphere.Sphere"));
-            break;
-
-        case EMaskBaseShape::Cube:
-            MeshToUse = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube.Cube"));
-            break;
-
-        case EMaskBaseShape::Cylinder:
-            MeshToUse = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cylinder.Cylinder"));
-            break;
-
-        case EMaskBaseShape::Capsule:
-            MeshToUse = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Capsule.Capsule"));
-            break;
-
-        case EMaskBaseShape::Custom:
-            MeshToUse = CustomBaseMesh;
-            break;
+        return;
     }
-
-    if (MeshToUse)
-    {
-        BaseMesh->SetStaticMesh(MeshToUse);
-    }
-}
-
-void AMaskActor::UpdateFaceOffsets()
-{
-    if (!BaseMesh || !FaceRoot) return;
 
     FVector Forward = GetActorForwardVector();
+    FVector Origin;
+    FVector Extent;
 
-    // Front surface offset
-    FVector Origin, Extent;
-    BaseMesh->GetLocalBounds(Origin, Extent);
-    float SphereRadius = Extent.X * BaseMesh->GetComponentScale().X;
-    FVector BaseOffset = Forward * SphereRadius;
+    BaseMesh->GetLocalBounds(Origin,Extent);
 
-    // -----------------------------
-    // Apply your working rotation to FaceRoot
-    // -----------------------------
-    FQuat YawQuat   = FRotationMatrix::MakeFromX(Forward).ToQuat();
-    FQuat PitchQuat = FQuat(FVector::RightVector, FMath::DegreesToRadians(90.f));
-    FQuat RollQuat  = FQuat(FVector::ForwardVector, FMath::DegreesToRadians(-90.f));
-
+    float Radius = Extent.X * BaseMesh->GetComponentScale().X;
+    FVector Position = Forward * Radius;
+    
+    FQuat YawQuat = FRotationMatrix::MakeFromX(Forward).ToQuat();
+    FQuat PitchQuat = FQuat(FVector::RightVector,FMath::DegreesToRadians(90.f));
+    FQuat RollQuat = FQuat(FVector::ForwardVector,FMath::DegreesToRadians(-90.f));
     FQuat FinalQuat = RollQuat * PitchQuat * YawQuat;
-
-    // Rotate FaceRoot itself
-    FaceRoot->SetRelativeRotation(FinalQuat);
-
-    FaceRoot->SetRelativeLocation(FaceRootOffset);
-
-    // -----------------------------
-    // Planes are now identity relative to FaceRoot
-    // -----------------------------
-    FQuat PlaneQuat = FQuat::Identity;
-
-    BrowsPlane->SetRelativeRotation(PlaneQuat);
-    EyesPlane->SetRelativeRotation(PlaneQuat);
-    NosePlane->SetRelativeRotation(PlaneQuat);
-    MouthPlane->SetRelativeRotation(PlaneQuat);
-    AccessoryPlane->SetRelativeRotation(PlaneQuat);
-    SymbolPlane->SetRelativeRotation(PlaneQuat);
-    DetailPlane->SetRelativeRotation(PlaneQuat);
-
-    // -----------------------------
-    // Position planes along FaceRoot local axes
-    // -----------------------------
-    FVector PlaneBasePos = Forward * ForwardSpacing; // small offset along sphere forward
-    BrowsPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,BrowsHeight));
-    EyesPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,EyesHeight));
-    NosePlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,NoseHeight));
-    MouthPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,MouthHeight));
-    AccessoryPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,AccessoryHeight));
-    SymbolPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,SymbolHeight));
-    DetailPlane->SetRelativeLocation(PlaneBasePos + FVector(0,0,DetailHeight));
-
-    // ================= SCALE =================
-    BrowsPlane->SetRelativeScale3D(FVector(GlobalFaceScale * BrowsScale));
-    EyesPlane->SetRelativeScale3D(FVector(GlobalFaceScale * EyesScale));
-    NosePlane->SetRelativeScale3D(FVector(GlobalFaceScale * NoseScale));
-    MouthPlane->SetRelativeScale3D(FVector(GlobalFaceScale * MouthScale));
-    AccessoryPlane->SetRelativeScale3D(FVector(GlobalFaceScale * AccessoryScale));
-    SymbolPlane->SetRelativeScale3D(FVector(GlobalFaceScale * SymbolScale));
-    DetailPlane->SetRelativeScale3D(FVector(GlobalFaceScale * DetailScale));
-
-    // Refresh render state
-    BrowsPlane->MarkRenderStateDirty();
-    EyesPlane->MarkRenderStateDirty();
-    NosePlane->MarkRenderStateDirty();
-    MouthPlane->MarkRenderStateDirty();
-    AccessoryPlane->MarkRenderStateDirty();
-    SymbolPlane->MarkRenderStateDirty();
-    DetailPlane->MarkRenderStateDirty();
-
-    // Make faces write to custom depth
-    BrowsPlane->SetRenderCustomDepth(true);
-    EyesPlane->SetRenderCustomDepth(true);
-    NosePlane->SetRenderCustomDepth(true);
-    MouthPlane->SetRenderCustomDepth(true);
-    AccessoryPlane->SetRenderCustomDepth(true);
-    SymbolPlane->SetRenderCustomDepth(true);
-    DetailPlane->SetRenderCustomDepth(true);
-
-    // Give them a unique stencil value
-    BrowsPlane->CustomDepthStencilValue = 1;
-    EyesPlane->CustomDepthStencilValue = 1;
-    NosePlane->CustomDepthStencilValue = 1;
-    MouthPlane->CustomDepthStencilValue = 1;
-    AccessoryPlane->CustomDepthStencilValue = 1;
-    SymbolPlane->CustomDepthStencilValue = 1;
-    DetailPlane->CustomDepthStencilValue = 1;
-
+    FacePlane->SetRelativeRotation(FinalQuat);
+    FacePlane->SetRelativeLocation(Position);
+    FacePlane->SetRelativeScale3D(FVector(FaceScale));
 }
 
+void AMaskActor::CreateFaceMaterial()
+{
+    if (!FaceMasterMaterial || !FacePlane)
+    {
+        return;
+    }
 
-// Swap a face part texture
+    UMaterialInterface* CurrentMat = FacePlane->GetMaterial(0);
+
+    if (!CurrentMat || CurrentMat->GetMaterial() != FaceMasterMaterial)
+    {
+        DynamicFaceMaterial =
+            UMaterialInstanceDynamic::Create(
+                FaceMasterMaterial,
+                this);
+
+        FacePlane->SetMaterial(0, DynamicFaceMaterial);
+    }
+    else
+    {
+        DynamicFaceMaterial =
+            Cast<UMaterialInstanceDynamic>(CurrentMat);
+
+        if (!DynamicFaceMaterial)
+        {
+            DynamicFaceMaterial =
+                UMaterialInstanceDynamic::Create(
+                    FaceMasterMaterial,
+                    this);
+
+            FacePlane->SetMaterial(0, DynamicFaceMaterial);
+        }
+    }
+}
+
+void AMaskActor::RefreshAllFaceParts()
+{
+    if (!DynamicFaceMaterial)
+    {
+        return;
+    }
+
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("BrowsTex"),BrowsTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("EyesTex"),EyesTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("NoseTex"),NoseTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("MouthTex"),MouthTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("AccessoryTex"),AccessoryTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("SymbolTex"),SymbolTexture);
+    DynamicFaceMaterial->SetTextureParameterValue(TEXT("DetailTex"),DetailTexture);
+}
+
 void AMaskActor::SetFacePart(EFacePartCategory Category, UTexture2D* FaceTexture)
 {
-    if (HasAnyFlags(RF_ClassDefaultObject)) return;
-    
-    if (!FaceTexture || !FaceMasterMaterial) return;
-
-    UStaticMeshComponent* TargetPlane = nullptr;
+    if (!DynamicFaceMaterial || !FaceTexture)
+    {
+        return;
+    }
 
     switch (Category)
     {
-    case EFacePartCategory::Brows:     TargetPlane = BrowsPlane;     BrowsTexture = FaceTexture; break;
-    case EFacePartCategory::Eyes:      TargetPlane = EyesPlane;      EyesTexture  = FaceTexture; break;
-    case EFacePartCategory::Nose:      TargetPlane = NosePlane;      NoseTexture  = FaceTexture; break;
-    case EFacePartCategory::Mouth:     TargetPlane = MouthPlane;     MouthTexture = FaceTexture; break;
-    case EFacePartCategory::Accessory: TargetPlane = AccessoryPlane; AccessoryTexture = FaceTexture; break;
-    case EFacePartCategory::Symbol:    TargetPlane = SymbolPlane;    SymbolTexture = FaceTexture; break;
-    case EFacePartCategory::Detail:    TargetPlane = DetailPlane;    DetailTexture = FaceTexture; break;
-    }
+        case EFacePartCategory::Brows:
+        {
+            BrowsTexture = FaceTexture;
 
-    if (!TargetPlane) return;
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("BrowsTex"),
+                FaceTexture);
 
-    // 🚨 Ensure material exists FIRST
-    if (TargetPlane->GetMaterial(0) != FaceMasterMaterial)
-    {
-        TargetPlane->SetMaterial(0, FaceMasterMaterial);
-    }
+            break;
+        }
 
-    // Now safe to create MID
-    UMaterialInstanceDynamic* MID =
-        Cast<UMaterialInstanceDynamic>(TargetPlane->GetMaterial(0));
+        case EFacePartCategory::Eyes:
+        {
+            EyesTexture = FaceTexture;
 
-    if (!MID)
-    {
-        MID = UMaterialInstanceDynamic::Create(FaceMasterMaterial, this);
-        TargetPlane->SetMaterial(0, MID);
-    }
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("EyesTex"),
+                FaceTexture);
 
-    if (MID)
-    {
-        MID->SetTextureParameterValue(TEXT("FaceTexture"), FaceTexture);
+            break;
+        }
+
+        case EFacePartCategory::Nose:
+        {
+            NoseTexture = FaceTexture;
+
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("NoseTex"),
+                FaceTexture);
+
+            break;
+        }
+
+        case EFacePartCategory::Mouth:
+        {
+            MouthTexture = FaceTexture;
+
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("MouthTex"),
+                FaceTexture);
+
+            break;
+        }
+
+        case EFacePartCategory::Accessory:
+        {
+            AccessoryTexture = FaceTexture;
+
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("AccessoryTex"),
+                FaceTexture);
+
+            break;
+        }
+
+        case EFacePartCategory::Symbol:
+        {
+            SymbolTexture = FaceTexture;
+
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("SymbolTex"),
+                FaceTexture);
+
+            break;
+        }
+
+        case EFacePartCategory::Detail:
+        {
+            DetailTexture = FaceTexture;
+
+            DynamicFaceMaterial->SetTextureParameterValue(
+                TEXT("DetailTex"),
+                FaceTexture);
+
+            break;
+        }
     }
 }
 
 void AMaskActor::ApplyHeadMaterial()
 {
-    if (!BaseMesh || !FaceBaseMaterial) return;
+    if (!BaseMesh)
+    {
+        return;
+    }
 
     if (!DynamicHeadMaterial)
     {
-        DynamicHeadMaterial =
-            UMaterialInstanceDynamic::Create(FaceBaseMaterial, this);
+        DynamicHeadMaterial = BaseMesh->CreateAndSetMaterialInstanceDynamic(0);
 
-        BaseMesh->SetMaterial(0, DynamicHeadMaterial);
+        BaseMesh->SetMaterial(
+            0,
+            DynamicHeadMaterial);
     }
 
     DynamicHeadMaterial->SetVectorParameterValue(
         TEXT("FaceColor"),
-        HeadColor
-    );
+        HeadColor);
 }
-
--- Source/TheOtleyRun/Public/MaskActor.h
+++ Source/TheOtleyRun/Public/MaskActor.h
@@ -2,197 +2,106 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Kismet/KismetMathLibrary.h"
-#include "Materials/MaterialInterface.h"
 #include "Materials/MaterialInstanceDynamic.h"
 #include "MaskActor.generated.h"
 
-// Enum for selecting base shape
-UENUM(BlueprintType)
-enum class EMaskBaseShape : uint8
-{
-    Sphere      UMETA(DisplayName = "Sphere"),
-    Cube        UMETA(DisplayName = "Cube"),
-    Cylinder    UMETA(DisplayName = "Cylinder"),
-    Capsule     UMETA(DisplayName = "Capsule"),
-    Custom      UMETA(DisplayName = "Custom Mesh")
-};
-
-// Enum for face part categories
 UENUM(BlueprintType)
 enum class EFacePartCategory : uint8
 {
-    Brows   UMETA(DisplayName = "Brows"),
-    Eyes    UMETA(DisplayName = "Eyes"),
-    Nose    UMETA(DisplayName = "Nose"),
-    Mouth   UMETA(DisplayName = "Mouth"),
-    Accessory UMETA(DisplayName = "Accessory"),
-    Symbol  UMETA(DisplayName = "Symbol"),
-    Detail  UMETA(DisplayName = "Detail")
+    Brows,
+    Eyes,
+    Nose,
+    Mouth,
+    Accessory,
+    Symbol,
+    Detail
 };
 
 UCLASS()
 class THEOTLEYRUN_API AMaskActor : public AActor
 {
     GENERATED_BODY()
-private:
-    bool bNeedsMaterialRefresh = false;
 
-public:	
-    // Sets default values
+public:
+
     AMaskActor();
-
-    // Called when properties change in editor or actor is spawned
-    virtual void OnConstruction(const FTransform& Transform) override;
-
-#if WITH_EDITOR
-    virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
-    virtual void PostRegisterAllComponents() override;
-#endif
-
-    UPROPERTY(EditDefaultsOnly, Category="00 Mask")
-    UMaterialInterface* FaceMasterMaterial;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* BrowsTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* EyesTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* NoseTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* MouthTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* AccessoryTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* SymbolTexture;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask|Faces")
-    UTexture2D* DetailTexture;
-
-    
-    // Swap a face part texture
-    UFUNCTION(BlueprintCallable, Category="00 Mask")
-    void SetFacePart(EFacePartCategory Category, UTexture2D* FaceTexture);
-    void ApplyHeadMaterial();
-
-    // Apply base shape mesh
-    UFUNCTION(BlueprintCallable, Category="00 Mask")
-    void ApplyBaseShape();
-
-    // Update face plane offsets based on mesh size
-    UFUNCTION(BlueprintCallable, Category="00 Mask")
-    void UpdateFaceOffsets();
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    bool bAutoLayout = true;
-
-    // --- Base shape selection ---
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="00 Mask")
-    EMaskBaseShape BaseShape = EMaskBaseShape::Sphere;
-
-    // Optional custom mesh for BaseShape = Custom
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="00 Mask")
-    UStaticMesh* CustomBaseMesh = nullptr;
-
-    // --- Components ---
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    USceneComponent* Root;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* BaseMesh;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    USceneComponent* FaceRoot;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* BrowsPlane;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* EyesPlane;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* NosePlane;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* MouthPlane;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* AccessoryPlane;
-    
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* SymbolPlane;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="00 Mask")
-    UStaticMeshComponent* DetailPlane;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float BrowsHeight = 0.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float EyesHeight = 0.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float NoseHeight;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float MouthHeight = 0.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float AccessoryHeight = 0.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float SymbolHeight = 0.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float DetailHeight = 0.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float ForwardSpacing = 0.05f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float GlobalFaceScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float EyesScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float NoseScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float MouthScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float BrowsScale = 1.f;
-
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float AccessoryScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float SymbolScale = 1.f;
-    
-    UPROPERTY(EditAnywhere, Category="00 Mask")
-    float DetailScale = 1.f;
-    
-    // Editor tweak for manually moving the face
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="00 Mask|Editor")
-    FVector FaceRootOffset = FVector::ZeroVector;
 
 protected:
 
-    // Base face material (assigned in editor)
-    UPROPERTY(EditDefaultsOnly, Category = "00 Mask|Material")
-    UMaterialInterface* FaceBaseMaterial = nullptr;
+    virtual void OnConstruction(const FTransform& Transform) override;
 
-    // Runtime dynamic material
-    UPROPERTY(Transient)
-    UMaterialInstanceDynamic* DynamicHeadMaterial = nullptr;
+#if WITH_EDITOR
+    virtual void PostEditChangeProperty(
+        FPropertyChangedEvent& PropertyChangedEvent) override;
+    virtual void PostLoad() override;
+#endif
 
-    // Skin / head color
-    UPROPERTY(EditAnywhere, Category="00 Mask|Color")
+public:
+
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    USceneComponent* Root;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    UStaticMeshComponent* BaseMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    UStaticMeshComponent* FacePlane;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Mask")
+    UStaticMesh* CustomBaseMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Materials")
+    UMaterialInterface* FaceMasterMaterial;
+
+    UPROPERTY()
+    UMaterialInstanceDynamic* DynamicFaceMaterial;
+
+    UPROPERTY()
+    UMaterialInstanceDynamic* DynamicHeadMaterial;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* BrowsTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* EyesTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* NoseTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* MouthTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* AccessoryTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* SymbolTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    UTexture2D* DetailTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Face")
+    float FaceScale = 1.f;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Head")
     FLinearColor HeadColor = FLinearColor::White;
+
+    UFUNCTION(BlueprintCallable)
+    void UpdateFaceTransform();
+
+    UFUNCTION(BlueprintCallable)
+    void ApplyHeadMaterial();
+
+    UFUNCTION(BlueprintCallable)
+    void CreateFaceMaterial();
+
+    UFUNCTION(BlueprintCallable)
+    void RefreshAllFaceParts();
+
+    UFUNCTION(BlueprintCallable)
+    void SetFacePart(
+        EFacePartCategory Category,
+        UTexture2D* FaceTexture);
 };
